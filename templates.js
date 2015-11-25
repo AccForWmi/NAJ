53674d3cb532ce75136e408a19de2f1bdd631003
AccForWmi <as15176@st.amu.edu.pl>
2015-11-25 09:42:20 +0100

    Create templates.js

---
+++ templates.js
@@ -0,0 +1,7 @@
+function getTodosTemplate(newTaskObj) {
+    return "<div class='todos-data'>" +
+                "<div class='todos-row'>" +
+                    newTaskObj.taskName + 
+                "</div>" +
+            "</div>";
+}
